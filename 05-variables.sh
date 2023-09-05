42db2ddfe7aab87fe5cd875c1519a3f8096a52ef
Anagi <nageswari.avula@gmail.com>
2023-09-05 14:24:33 +0530

    practice again

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,7 +4,7 @@ echo "please enter your user name"
 
 read -s USERNAME # this is the variable where the value of the  user 1 entered
 
-
+# this is only for vaidation, we should not print in the terminal
 echo "Username entered is : $USERNAME"
 
 echo "please enter your user Password"
@@ -12,4 +12,4 @@ echo "please enter your user Password"
 read -s PASSWORD
 
 echo "Passsword entered is : $PASSWORD"
-# this is only for vaidation, we should not print in the terminal
+
---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# this is called array, instead of single value it can hold multiple values
+PERSONS=("Ramesh" "Suresh" "Sachin")
+# inside array we always refer values with index and it is starts from 0
+
+echo "first person: ${PERSONS[0]}"
+
+echo "all  persons: ${PERSONS[@]}"
