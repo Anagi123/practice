915422bc7eded5ab13059a64b844a3accced03de
Anagi <nageswari.avula@gmail.com>
2023-09-05 14:26:30 +0530

    practice again

--- 06-array.sh
+++ 06-array.sh
@@ -4,5 +4,6 @@
 PERSONS=("Ramesh" "Suresh" "Sachin")
 # inside array we always refer values with index and it is starts from 0
 
-echo "first person: ${PERSONS[0]}"
 
+
+echo "all  persons: ${PERSONS[@]}"
