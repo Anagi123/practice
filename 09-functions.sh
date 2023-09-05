87e84e5193cb85ae649e4c10d11103c187f8e756
Anagi <nageswari.avula@gmail.com>
2023-09-05 17:44:58 +0530

    practice again

---
+++ 09-functions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#our program goal is to install mysql
+
+USERID=$(id -u)
+# this function should validate the previous commond and inform the user either success or failure 
+VALIDATE(){
+    # $1--->it will receive the argument 1
+    if [ $1 -ne 0 ]
+    then 
+        echo "installation ... FAILURE"
+    else
+        echo "installation ... SUCCESS"  
+    fi      
+}
+
+if [ $USERID -ne 0 ]
+
+then 
+    echo "ERROR::Please run this script with root access"
+    exit 1
+# else
+#     echo "INFO::you are root user"
+fi    
+
+# it is our responsibility again to check installation is success or not
+yum install mysql -y
+
+VALIDATE $?
+
+yum install postfix -y
+
+VALIDATE $?
