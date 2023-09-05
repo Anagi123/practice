dcadb7793896809653aadfa083369675321bd462
Anagi <nageswari.avula@gmail.com>
2023-09-05 12:14:41 +0530

    practice again

---
+++ 05-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "please enter your user name"
+
+read -s USERNAME # this is the variable where the value of the 1 user entered
+
+#this is only for vaidation, we should not print in the terminal
+echo "Username entered is : $USERNAME"
+
+echo "please enter your user Password"
+
+read -s PASSWORD
+
+echo "Passsword entered is : $PASSWORD"
