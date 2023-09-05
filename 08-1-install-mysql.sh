d054b119d24044a9328be1bdac79dcc4c1317ce2
Anagi <nageswari.avula@gmail.com>
2023-09-05 15:28:40 +0530

    practice again

--- 08-1-install-mysql.sh
+++ 08-1-install-mysql.sh
@@ -5,7 +5,6 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-
 then 
     echo "ERROR::Please run this script with root access"
     exit 1
@@ -14,5 +13,12 @@ then
 fi    
 
 # it is our responsibility again to check installation is success or not
-yum install mysqlllll -y
+yum install mysql -y
 
+if [$? ne 0]
+then 
+    echo"installation of mysql is error"
+    exit 1
+else
+    echo"installation of mysql is success"    
+fi
